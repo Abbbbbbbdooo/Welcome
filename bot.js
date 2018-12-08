1533b426001527ca5d6d16235015a8dbac88d537
Abdo <45042294+Abbbbbbbdooo@users.noreply.github.com>
2018-12-08 21:06:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ const Discord = require('discord.js'),
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("516561376324354049");
+const mohamed= member.guild.channels.get("509779469234601984");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`Welcome To Server *Magic*`), 4000)        
+setTimeout(() => mohamed.send(`Welcome To Server **DetermindGuild**`), 4000)        
 }
 });
 
